c1b00a74a246d8a085e746c3abd94f58b275ced4
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-07-16 15:33:03 -0700

    Starting to create user views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @quiz = Quiz.all.shuffle
+    # if current_user
+  #   @user = current_user
+  #    @games = @user.games.order('created_at DESC').limit(5);
   end
 end
