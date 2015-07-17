7a5927b2d31f3dc8e5da1182e4ed434e7ad26dc8
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-07-17 16:53:44 -0700

    Add initialize_on_precompile = false in an odd attempt to get postgres working on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.initialize_on_precompile = false
+
 end
