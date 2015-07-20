73d158221683ef65a7c1b8e7b41ec7670f56f626
WILL LOCKWOOD <WRLOCKWOOD1@GMAIL.COM>
2015-07-20 15:59:46 -0700

    Minor fixes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @quiz = Quiz.all.shuffle
+    @quizzes = Quiz.all.order('games_count desc').limit(10)
     if current_user
       @user = current_user
       @games = @user.games.order('created_at DESC').limit(5)
-      @quizzes = Quiz.all.order('games_count desc').limit(10)
     end
   end
 end
