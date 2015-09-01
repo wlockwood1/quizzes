80817eb4419dca6cad670dc3bbb50967d4cfcd01
Will Lockwood <will.lockwood@tracktrain.com>
2015-08-31 22:22:24 -0700

    Try again

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Quizzes
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.react.variant      = :production
     config.react.addons = true
     # config/action_controller/permit_all_parameters = true
     # config.serve_static_assets = true
